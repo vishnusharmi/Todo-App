5f1cfbf256e207fb6176bb179358932034cb1ce2
vishnusharmi <bandarivishnusharmi9390@gmail.com>
2023-07-03 12:43:59 +0530

    fixed errors

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from "react";
 
 import AssignmentTodo from "./assignment/AssignmentTodo";
-import Todo from "./practice/Todo";
-
-export const UserContext = React.createContext();
 
 const App = () => {
   return (
     <div>
       <AssignmentTodo />
-      {/* <Todo /> */}
     </div>
   );
 };
